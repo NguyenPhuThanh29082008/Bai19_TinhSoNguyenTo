516b529c927d86d80a59af7bc99d2e9fb1804fcb
NguyenPhuThanh20082008 <nguyenphuthanhpch298@gmail.com>
2024-11-25 10:42:02 +0700

    Add project files.

---
+++ Bai19_TinhSoNguyenTo.cpp
@@ -0,0 +1,32 @@
+// Bai19_TinhSoNguyenTo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Kiem tra so co phai la so nguyen to hay khong\n";
+	int so;
+	cout << "Nhap so: "; cin >> so;
+	int dem = 0;
+
+	for (int i = 1; i <= so; i++)
+	{
+		if (so %i == 0)
+		{
+			dem++;
+		}
+	}
+	cout << "So dem = " << dem << endl;
+	if (dem == 2)
+	{
+		cout << "So " << so << " La so nguyen to\n";
+	}
+	else
+	{
+		cout << "So " << so << " Khong phai la so nguyen to\n";
+	}
+
+
+	return 0;
+}
